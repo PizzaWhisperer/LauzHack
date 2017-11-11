f4d1bc0a8678f5e9c63d69f1b02f09ededd105a4
Mathilde Raynal <mathilde.raynal@epfl.ch>
2017-11-11 22:21:33 +0100

    Simple but working

--- Beat Craft/Assets/_Scripts/AudioScript.cs
+++ Beat Craft/Assets/_Scripts/AudioScript.cs
@@ -15,69 +15,55 @@ public class AudioScript : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+
         records = new List<float>[26];
         int i;
         for (i = 0; i < 26; i++)
         {
             sources[i].clip = clips[i];
-            //sources[i].loop = true;
-        }      
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        if (Input.GetKeyDown("Space")) {
-            StartCoroutine("Record");
-        }
-                 /*
-                 * We just play without recording (we erease the beat of that key)
-                 * Key to index mapping : 
-                 * A = 0; B = 1 ..... Z = 25; 
-                 *        
-                 * If pressed twice the beat stops
-                 */
-
-         foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
-         {
-            if (Input.GetKeyDown(kcode))
-            {
-                int index = (int)kcode - 97;
-
-                if (index >= 0 && index <= 26)
-                {
-                    
-                    sources[index].clip = clips[index];
-                    sources[index].Play();
-                   
-                }
-                if (index == (27 - 97))
-                { //we pressed ESC
-                    int i;
-                    for (i = 0; i < 26; i++)
-                    {
-                        sources[i].Stop();
-                    }
-                }
-            }
-         }
-
-        
-        /*if (/*Dial is pressed) {
-          //clip = Microphone.Start("Built-in Microphone", true, 6, 44100);
-          //we update sources[index].clip = clip;
+            records[i] = new List<float>();
         }
 
-        if (Dial is sofly pressed)
-        {
-          //clip = Microphone.Start("Built-in Microphone", true, 3, 44100);
-          //we update sources[index].clip = clip;
-        }*/
+        StartCoroutine(Record(records));
+        StartCoroutine(Play(records));
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+       /*
+       * We just play without recording (we erease the beat of that key)
+       * Key to index mapping : 
+       * A = 0; B = 1 ..... Z = 25; 
+       *        
+       * If pressed twice the beat stops
+       */
+       foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+       {
+          if (Input.GetKeyDown(kcode))
+          {
+            int index = (int)kcode - 97;
+              if (index >= 0 && index <= 26)
+              {
+                sources[index].clip = clips[index];
+                sources[index].Play();
+              }
+              if (index == (27 - 97))
+              { //we pressed ESC
+                int i;
+                for (i = 0; i < 26; i++)
+                {
+                  sources[i].Stop();
+                }
+              }
+          }
+       }
 
-    IEnumerator Record() {
-        while (Input.GetKeyDown("Space"))
+      
+    }
+
+    IEnumerator Record(List<float>[] records) {
+        while (Input.GetKeyDown("space"))
         {
             foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
             {
@@ -97,7 +83,22 @@ public class AudioScript : MonoBehaviour {
                     }
                 }
             }
+            yield return null;
         }
-        yield return null;
+    }
+
+    IEnumerator Play(List<float>[] records){
+        int i;
+        for (i = 0; i < 26; i++) {
+            List<float> list = records[i];
+            int j;
+            for (j = 0; j < list.Count; j++) {
+                float time = list[j];
+
+            }
+        
+
+            yield return null;
+        }
     }
 }
