213de42d7648fd8a9fbfcc139295b0357503d5cd
Mathilde Raynal <mathilde.raynal@epfl.ch>
2017-11-11 18:09:19 +0100

    Audios

--- Beat Craft/Assets/_Scripts/AudioScript.cs
+++ Beat Craft/Assets/_Scripts/AudioScript.cs
@@ -1,23 +1,57 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioScript : MonoBehaviour {
+    
+    [Header("Audio sources")]
+    public AudioSource[] sources;
+    [Header("Audio clips")]
+    public AudioClip[] clips;
 
-    public AudioClip MusicClip;
+    private bool[] playing;
 
-    public AudioSource MusicSource;
+    private AudioSource MusicSource;
 
-    	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        int i;
+        for (i = 0; i < 27; i++)
+        {
+            sources[i] = gameObject.GetComponent<AudioSource>();
+            sources[i].clip = clips[i];
+            playing[i] = false;
+        }
 
-        MusicSource.clip = MusicClip;
         
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
-            MusicSource.Play();
+        /*
+         * Key to index mapping : 
+         * A = 0; B = 1 ..... Z = 25; Space = 26; 
+         * If Esc pressed we stop everything
+         * 
+         * If pressed twice the beat stops
+         */
+
+        foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(kcode))
+                Console.WriteLine(kcode);
+            Console.WriteLine((int)kcode);
+                //kcode is matched to (int)kcode ?
+                if (playing[(int)kcode])
+                {
+                    sources[(int)kcode].Stop();
+                    playing[(int)kcode] = false;
+                }
+                else {
+                    sources[(int)kcode].Play();
+                    playing[(int)kcode] = true;
+                } 
+        }
 	}
 }
---
+++ Beat Craft/Assets/_Scripts/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    private SteamVR_Controller.Device device;
+    private SteamVR_TrackedObject trackedObj;
+
+    public GameObject keyboard;
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger)) {
+            keyboard.transform.position = gameObject.transform.position;
+        }
+    }
+}
