20467d821d102175e48e77732a4c212e59bfe10f
Mathilde Raynal <mathilde.raynal@epfl.ch>
2017-11-12 03:12:46 +0100

    MEH

--- Beat Craft/Assets/_Scripts/AudioScript.cs
+++ Beat Craft/Assets/_Scripts/AudioScript.cs
@@ -11,6 +11,7 @@ public class AudioScript : MonoBehaviour {
     public AudioClip[] clips;
 
     private List<float>[] records;
+    private float lastRecordTime;
     private AudioSource MusicSource;
 
     // Use this for initialization
@@ -23,21 +24,21 @@ public class AudioScript : MonoBehaviour {
             sources[i].clip = clips[i];
             records[i] = new List<float>();
         }
+        lastRecordTime = -1f;
 
-       // StartCoroutine(Record(records));
-        StartCoroutine(Play(records));
-    }
+        //StartCoroutine(Play(records));
+        StartCoroutine(Record(records));
+       
+        }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Update ?");
        /*
        * We just play without recording
        * Key to index mapping : 
        * A = 0; B = 1 ..... Z = 25; 
-       *        
-       * If pressed twice the beat stops
+       *  
        */
        foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
        {
@@ -53,46 +54,93 @@ public class AudioScript : MonoBehaviour {
                 int i;
                 for (i = 0; i < 26; i++)
                 {
+                  records[i].Clear(); //we erease all the beats
                   sources[i].Stop();
                 }
               }
           }
        }
+       
+            
+        
 
-      
     }
 
     IEnumerator Record(List<float>[] records)
     {
-        //while(true) {
-            //new WaitForSeconds(5);
+        //while (true)
+        //{
             while (Input.GetKey("space"))
             {
-                Debug.Log("Recording");
+                Debug.Log("WE PRESSED");
                 foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
                 {
-                    int? index = null;
-                    if (Input.GetKeyDown(kcode) && (!index.HasValue || ((int)kcode - 97) == index))
+                    if (Input.GetKeyDown(kcode) && ((int)kcode != 32) && (Time.time - lastRecordTime) > 0.5f)
                     {
-                        if (!index.HasValue)
-                        {
-                            records[(int)index].Clear(); //we earse the previous record
-                            index = (int)kcode - 97;
-                            records[(int)index].Add(Time.time);
-                        }
-                        else
-                        {
-                            float prev = records[(int)index][records[(int)index].Count - 1];
-                            records[(int)index].Add(Time.time - prev);
-                        }
+                        records[(int)kcode - 97].Add(Time.time - lastRecordTime);
+                        lastRecordTime = Time.time;
+                        Debug.Log("Storing");
                     }
-                }
-                yield return null;
+                }  
             }
-        yield return new WaitForSeconds(0.3f);
-        //}
+            yield return null;
+       // }
     }
 
+    
+        /*Debug.Log("Recording");
+        if (Input.GetKey("space"))
+        {
+            Debug.Log("RecordingBIS");
+            while (Input.GetKey("space"))
+            {
+                bool start = true;
+                float timeStart = Time.time;
+                foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+                {
+                    if (Input.GetKeyDown(kcode) && ((int)kcode) != 32)
+                    {
+                        if (start)
+                        {
+                            Debug.Log("ONCE ?");
+                            start = false;
+                            records[(int)kcode - 97].Clear();
+                        }
+                        records[(int)kcode - 97].Add(Time.time - timeStart);
+                    }
+                    yield return null;
+                }
+            }
+        }
+
+    
+        /*new WaitForSeconds(5);
+        while (Input.GetKey("space"))
+        {
+            Debug.Log("Recording");
+            foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+            {
+                int? index = null;
+                if (Input.GetKeyDown(kcode) && (!index.HasValue || ((int)kcode - 97) == index))
+                {
+                    if (!index.HasValue)
+                    {
+                        records[(int)index].Clear(); //we earse the previous record
+                        index = (int)kcode - 97;
+                        records[(int)index].Add(Time.time);
+                    }
+                    else
+                    {
+                        float prev = records[(int)index][records[(int)index].Count - 1];
+                        records[(int)index].Add(Time.time - prev);
+                    }
+                }
+            }
+            yield return null;
+        }
+    yield return new WaitForSeconds(0.3f);
+    //}*/
+    
     IEnumerator Play(List<float>[] records)
     {
         while (true)
@@ -100,22 +148,16 @@ public class AudioScript : MonoBehaviour {
             int i;
             for (i = 0; i < 26; i++)
             {
-                Debug.Log(i);
                 List<float> list = records[i];
+               
                 int j;
                 for (j = 0; j < list.Count; j++)
                 {
-                    Debug.Log("j");
-                    Debug.Log(j);
+                  
                     yield return new WaitForSeconds(list[j]);
-                    sources[j].Play();
+                    sources[i].Play();
                 }
                 yield return null;
-            }
-
-            if (Input.GetKey("space"))
-            {
-                StartCoroutine(Record(records));
             }
         }
     }
