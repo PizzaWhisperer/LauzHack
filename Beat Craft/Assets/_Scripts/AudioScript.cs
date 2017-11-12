799772ae075dde7fc6b622092ec811fc0de87705
Ekaterina Dopiro <ekaterina.dopiro@epfl.ch>
2017-11-12 03:09:32 +0100

    First Step
    Our best working loop

--- Beat Craft/Assets/_Scripts/AudioScript.cs
+++ Beat Craft/Assets/_Scripts/AudioScript.cs
@@ -4,18 +4,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioScript : MonoBehaviour {
-    
+
     [Header("Audio sources")]
     public AudioSource[] sources;
     [Header("Audio clips")]
     public AudioClip[] clips;
 
+    public struct Record
+    {
+        public int sound;
+        public float delay;
+
+        public Record(int p1, float p2)
+        {
+            sound = p1;
+            delay = p2;
+        }
+    }
+
+    private List<Record> loopToPlay;
     private List<float>[] records;
     private AudioSource MusicSource;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
 
+        loopToPlay = new List<Record>();
         records = new List<float>[26];
         int i;
         for (i = 0; i < 26; i++)
@@ -24,99 +38,121 @@ public class AudioScript : MonoBehaviour {
             records[i] = new List<float>();
         }
 
-       // StartCoroutine(Record(records));
+        loopToPlay.Add(new Record(-1, Time.time));
+        loopToPlay.Add(new Record(1, 1.5f));
+        //loopToPlay.Add(new Record(4, 0.5f));
+        //records[0] = new List<float>()
+        //{
+        //    0f, 1f, 2f
+        //};
+
+        // StartCoroutine(Record(records));
         StartCoroutine(Play(records));
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Update ?");
-       /*
-       * We just play without recording
-       * Key to index mapping : 
-       * A = 0; B = 1 ..... Z = 25; 
-       *        
-       * If pressed twice the beat stops
-       */
-       foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
-       {
-          if (Input.GetKeyDown(kcode))
-          {
-            int index = (int)kcode - 97;
-              if (index >= 0 && index <= 26)
-              {
-                sources[index].Play();
-              }
-              if (index == (27 - 97))
-              { //we pressed ESC
-                int i;
-                for (i = 0; i < 26; i++)
-                {
-                  sources[i].Stop();
-                }
-              }
-          }
-       }
-
-      
-    }
-
-    IEnumerator Record(List<float>[] records)
-    {
-        //while(true) {
-            //new WaitForSeconds(5);
-            while (Input.GetKey("space"))
+        // Debug.Log("Update ?");
+        /*
+        * We just play without recording
+        * Key to index mapping : 
+        * A = 0; B = 1 ..... Z = 25; 
+        *        
+        * If pressed twice the beat stops
+        */
+        foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(kcode))
             {
-                Debug.Log("Recording");
-                foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+
+                int index = (int)kcode - 97;
+                if (index >= 0 && index <= 26)
                 {
-                    int? index = null;
-                    if (Input.GetKeyDown(kcode) && (!index.HasValue || ((int)kcode - 97) == index))
+                    sources[index].Play();
+                }
+                if (index == (27 - 97))
+                { //we pressed ESC
+                    int i;
+                    for (i = 0; i < 26; i++)
                     {
-                        if (!index.HasValue)
-                        {
-                            records[(int)index].Clear(); //we earse the previous record
-                            index = (int)kcode - 97;
-                            records[(int)index].Add(Time.time);
-                        }
-                        else
-                        {
-                            float prev = records[(int)index][records[(int)index].Count - 1];
-                            records[(int)index].Add(Time.time - prev);
-                        }
+                        sources[i].Stop();
+                        loopToPlay.Clear();
                     }
                 }
-                yield return null;
-            }
-        yield return new WaitForSeconds(0.3f);
-        //}
-    }
 
-    IEnumerator Play(List<float>[] records)
-    {
-        while (true)
-        {
-            int i;
-            for (i = 0; i < 26; i++)
-            {
-                Debug.Log(i);
-                List<float> list = records[i];
-                int j;
-                for (j = 0; j < list.Count; j++)
+                //---------------------------------------------------------------------------
+                
+                if (Input.GetKey("space"))
                 {
-                    Debug.Log("j");
-                    Debug.Log(j);
-                    yield return new WaitForSeconds(list[j]);
-                    sources[j].Play();
-                }
-                yield return null;
-            }
+                    if ((int)kcode != 32)
+                    {
+                        Debug.Log((int)kcode);
+                        int indexRecDash = (int)kcode - 97;
+                        float t = Time.time;
+                        //Fix the xtart record time
+                        if (loopToPlay.Count == 0)
+                        {
+                            loopToPlay.Add(new AudioScript.Record(-1, t));
+                        }
+                        float del = Time.time - loopToPlay[0].delay;
+                        Debug.Log(del);
+                        loopToPlay.Add(new Record(indexRecDash, del));
+                        //Check if we need to insert
+                        /*
+                        if (loopToPlay[0].delay + loopToPlay[loopToPlay.Count - 1].delay < t + del)
+                        {
+                            loopToPlay.Add(new Record(indexRecDash, del));
+                        } else
+                        {
+                            bool done = false;
+                            int i = 1;
+                            while( i < loopToPlay.Count && !done)
+                            {
+                                if (loopToPlay[0].delay + loopToPlay[i].delay < t + del)
+                                {
+                                    loopToPlay.Insert(i, new Record(indexRecDash, del));
+                                    done = true;
+                                }
+                            }
+                        }
+                        Record last = loopToPlay[loopToPlay.Count - 1];
+                        Debug.Log(last.sound);
+                        Debug.Log(last.delay);*/
+                        //loopToPlay.Add(new Record(indexRecDash, del));
+                    }
 
-            if (Input.GetKey("space"))
-            {
-                StartCoroutine(Record(records));
+                } 
+
             }
         }
     }
+
+
+
+
+        IEnumerator Play(List<float>[] records)
+        {
+            while (true)
+            {
+                if (loopToPlay.Count != 0)
+                {
+                Record[] tempCopy = new Record[loopToPlay.Count];
+                loopToPlay.CopyTo(tempCopy);
+                    foreach (var rec in tempCopy)
+                    {
+                        if (rec.sound != -1)
+                        {
+                            yield return new WaitForSeconds(rec.delay);
+                            sources[rec.sound].Play();
+                        }
+                    }
+                    loopToPlay[0] = new Record(-1, Time.time);
+                }
+               
+                
+            }
+        }
+
 }
+
